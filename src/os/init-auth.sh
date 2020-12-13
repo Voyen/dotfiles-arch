4687aadfe4d4dc09131df5449ea436650891eeb1
Voyen <dev@voyen.io>
2020-12-13 13:11:41 +1030

    debug 2

--- src/os/init-auth.sh
+++ src/os/init-auth.sh
@@ -6,10 +6,11 @@ main() {
 
   print_in_purple "\n • Initialise Authentication\n\n"
 
+  echo "init git repo pwd1: $(pwd)"
   if cmd_exists "git"; then
 
     if [ "$(git config --get remote.origin.url)" != "$DOTFILES_ORIGIN" ]; then
-      echo "init git repo pwd: $(pwd)"
+      echo "init git repo pwd2: $(pwd)"
       ./initialise-git-repository.sh "$DOTFILES_ORIGIN"
     fi
 
