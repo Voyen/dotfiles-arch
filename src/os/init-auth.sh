d4764e3111a206d2a70c65c1117a13709dd827dc
Voyen <dev@voyen.io>
2020-12-13 13:40:46 +1030

    Revert "debug 2"
    This reverts commit 4687aadfe4d4dc09131df5449ea436650891eeb1.

--- src/os/init-auth.sh
+++ src/os/init-auth.sh
@@ -6,11 +6,10 @@ main() {
 
   print_in_purple "\n • Initialise Authentication\n\n"
 
-  echo "init git repo pwd1: $(pwd)"
   if cmd_exists "git"; then
 
     if [ "$(git config --get remote.origin.url)" != "$DOTFILES_ORIGIN" ]; then
-      echo "init git repo pwd2: $(pwd)"
+      echo "init git repo pwd: $(pwd)"
       ./initialise-git-repository.sh "$DOTFILES_ORIGIN"
     fi
 
